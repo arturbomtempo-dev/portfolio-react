861fd4e61ca5aaa45db8661b7ff5351ac5684af6
Artur Bomtempo Colen <abcolen@sga.pucminas.br>
2025-11-25 18:03:41 -0300

    refactor: reorganize imports in Container component and implement smooth scrolling on route change

--- src/components/app-container/index.tsx
+++ src/components/app-container/index.tsx
@@ -1,10 +1,21 @@
 import { Toaster as Sonner } from '@/components/ui/sonner';
 import { Toaster } from '@/components/ui/toaster';
-import { Loader } from '../loader';
-import { Layout } from '../layout';
 import { AppRoutes } from '@/routes';
+import { useEffect } from 'react';
+import { useLocation } from 'react-router-dom';
+import { Layout } from '../layout';
+import { Loader } from '../loader';
 
 export function Container() {
+    const { pathname } = useLocation();
+
+    useEffect(() => {
+        window.scrollTo({
+            top: 0,
+            behavior: 'smooth',
+        });
+    }, [pathname]);
+
     return (
         <>
             <Loader />
