1c257b9bba2d082c8a130bdc68a55afbf1878b8b
Artur Bomtempo Colen <arturbcolen@gmail.com>
2025-11-28 01:06:55 -0300

    fix: ensure build configuration includes chunk size warning limit

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,9 @@ import react from '@vitejs/plugin-react-swc';
 import path from 'path';
 
 export default defineConfig({
+    build: {
+        chunkSizeWarningLimit: 1000,
+    },
     server: {
         host: '::',
         port: 8080,
